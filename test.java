10cec3fdddd5aba9adb335bd0144551e864423fa
Magnus Karström <magnus@yesweb.se>
2022-01-27 14:10:28 +0100

    uppgift 1.9 sidan 25

---
+++ test.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hej!" + System.getProperty("user.name") + "!\n"
+        +"Du bor på adressen:\nKöpmannagatan 1b\n916 31 Bjurholm\nSverige");
+    }
+}
